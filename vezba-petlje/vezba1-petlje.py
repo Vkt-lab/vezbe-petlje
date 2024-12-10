0635768a9a83b1f540167e3404d630ca6336ef63
Djordje Uskokovic <djordje.uskokovic@link.co.rs>
2024-12-10 18:22:09 +0100

    dodat prvi fajl

---
+++ vezba-petlje/vezba1-petlje.py
@@ -0,0 +1,17 @@
+pocetna_pozicija = 0
+cilj = 50
+trenutna_pozicija = 0 
+brzina = 10
+
+
+for x in range (10):
+    print(trenutna_pozicija)
+    if trenutna_pozicija == cilj:
+        print("Stigao do cilja.")
+        break
+    elif trenutna_pozicija > cilj:
+        print("Prosao cilj.")
+    elif trenutna_pozicija < cilj:
+        print("Niste jos stigli.")
+    trenutna_pozicija += brzina
+
